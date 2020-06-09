e8948b36af0f585fe857c93b16d5cc88d2104a4d
Daniel Samson Raj <danielsam458@gmail.com>
2020-06-09 14:38:20 +0530

    Day-10

---
+++ Is-Subsequence.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    
+    bool check(vector<bool> arr, int index) {
+        for(int i=0;i<index;i++) {
+            if(arr[i]) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    bool isSubsequence(string s, string t) {
+        if(s.size() == 0 ) return true;
+        if(s.size() != 0 and t.size() == 0) return false;
+        vector<vector<bool>> dp(s.size(), vector<bool> (t.size(), false));
+        for(int i=0;i<s.size();i++) {
+            for(int j=0;j<t.size();j++) {
+                if(s[i] == t[j]) {
+                    if(i==0) {
+                        dp[i][j] = true;
+                    }
+                    else if(check(dp[i-1],j)) {
+                        dp[i][j] = true;
+                    }
+                }
+            }
+        }
+        for(auto it : dp[s.size()-1]) {
+            if(it) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
